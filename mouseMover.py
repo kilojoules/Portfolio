d302b1980ddc7d2c7aefb9ccb236fc0ae6d98399
kilojoules <jaq8@humboldt.edu>
2014-09-06 02:00:06 -0700

    Program to keep computer active

--- mouseMover.py
+++ mouseMover.py
@@ -1,17 +1,15 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 #JULIAN QUICK
-#"Daemon" to move mouse slowly, stopping unwanted power save effects
+#"Daemon" to move mouse slowly, stopping 
+#unwanted power save effects
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 import time
-
 mo=PyMouse()
 
+
 while True:
-<<<<<<< HEAD
-  time.sleep(.1)
-  mo.move(mo.position()[0]+11,mo.position()[1]+11)
-=======
+  #Every 300 seconds move the mouse 
+  #one pixel down and to the right
   time.sleep(300)
   mo.move(mo.position()[0]+1,mo.position()[1]+1)
->>>>>>> 0970849bfaf5331691ebc30dc29b8b34d3ac49c4

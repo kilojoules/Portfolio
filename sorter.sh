30de01b987c3c4e7be23f04532107ea76f6be415
kilojoules <jaq8@humboldt.edu>
2014-09-15 21:17:13 -0700

    Here is a sorting program I worte for my job as a grader!

---
+++ sorter.sh
@@ -0,0 +1,26 @@
+# Julian Quick
+# Description: sorts student submissions by creating individual student lab directories 
+# conainin the submission and grading rubric
+
+# This is set up for a direcotry containing: 
+#    The grading rubric spreadsheet as rubric
+#    A directory called "Submissions" containing lab submissions
+
+# replace spaces in filenames within Submissions with _
+detox -r Submissions
+
+# iterate through files in submissions
+for f in `ls Submissions`
+do
+    # cut first 20 letters from submission name
+    dir=`echo "$f"|cut -c 1-20`
+
+    # new directory with that name
+    mkdir -p "$dir"
+
+    # copy that submission to the new directory,
+    # as well as grading rubric
+    cp Submissions/"$f" "./$dir"
+    cp grader.py "$dir"
+    tar -C $dir -zxvf $dir/$f
+done

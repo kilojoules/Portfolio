57faa4c5f6d65fdc1d426d20733af5d6453355f1
kilojoules <jaq8@humboldt.edu>
2014-09-16 01:10:35 -0700

    moves mouse to avoid unwanted energy savings effects

--- mouseMover.py
+++ mouseMover.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-#JULIAN QUICK
-#"Daemon" to move mouse slowly, stopping 
-#unwanted power save effects
+# JULIAN QUICK
+# "Daemon" to move mouse slowly, stopping 
+# unwanted power save effects
 from pymouse import PyMouse
 import time
 mo=PyMouse()
 
 
 while True:
-  #Every 300 seconds move the mouse 
-  #one pixel down and to the right
+  # Every 300 seconds move the mouse 
+  # one pixel down and to the right
   time.sleep(300)
   mo.move(mo.position()[0]+1,mo.position()[1]+1)

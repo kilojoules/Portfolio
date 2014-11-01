b140d7c1154df2c0098384ecf04e29c65083c677
kilojoules <jaq8@humboldt.edu>
2014-10-31 23:37:51 -0700

    Making Grading Simple. Added hand work function.

--- Grading_Automation/rubric.py
+++ Grading_Automation/rubric.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python
 # Julian Quick
-#
-# Requests info for Engr325 Fortran Programming Rubric
-# ======================================================
+# This program is used to help me grade E325 assignments
+
+def handProb(name):
+   print "how was problem "+name+"?"
+   ans = raw_input("how was problem "+name+"? __ / 100")
+   print >>outfile, name+':'
+   print >>otfile, "   Score: "+str(ans)+'/100'
+   return(ans)
+
 # req_info asks user for grade, prints result to output grading file, and 
 # returns the grade value
-# VARIABLE DICTIONARY:
-#   ans: user input
-#   half 1: prompt untill score fraction
-#   half2: prompt after and including score fraction
 def req_info(half1,half2,outfile):
-  
-   # request info
    ans=raw_input(str(half1)+str(half2))
-
-   # write results to output file
    print >>outfile,half1+str(ans)+half2
-
    return int(ans)
 
-# req_info asks the user to grade an assigned program 
-# using the progrramming grading rubric
-# VARIABLE DICTIONARY
-#    num: name of program being evaluated
-#    output: output feedback file
+# reqinf uses req)info to request info based on Engr325 Fortran Programming Rubric
 def reqinf(num,output):
-
-  # Create program specific feedback heading
   print >>output, 'PROGRAM '+str(num)+':'
-
-  # request critereon grades, add to score
   score=0
   print 'how was Program '+str(num)+'\'s...'
   score+=req_info('Program Description ','/10: What does the program do and how?',output)
@@ -41,27 +30,15 @@ def reqinf(num,output):
 
   # Ask for comments
   coms=str(raw_input('comments?'))
-
-  # Add whitespaces to output file
   print >>output,''
   print >>output, 'Score is '+str(score)+'/100'
   print >>output,''
   print >>output,'Comments: '+coms
   print >>output,''
   print >>output,''
-
-  # Return assignment's totla score out of 100
   return score
 
-# ======================================================
-
-# Open output file
 rubric=open('rubric.txt','w')
-
-# Determine score
 score=0
-score+=reqinf('ch.6 problem 7',rubric)
-score+=reqinf('ch.7 problem 9',rubric)
-
-# Final score is average score
-print >>rubric,'Total Score is ',score/2
+score+=reqinf('Geometric Mean',rubric)
+print >>rubric,'Total Score is ',score

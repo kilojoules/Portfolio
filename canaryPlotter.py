73ed7f02cb85c6611a2ae926ad67c6161c49ea84
kilojoules <jaq8@humboldt.edu>
2014-11-04 00:33:09 -0800

    added error checking

--- canaryPlotter.py
+++ canaryPlotter.py
@@ -1,23 +1,33 @@
-# Julian QUick
+#!/usr/bin/env python
+# Julian Quick
 # October 25 2014
-
 # plots first column of csv file as x axis, next 5 columns as y axis
 
 from pandas import *
 import matplotlib.pyplot as plt
 import sys
 
+# This program needs a csv file to plot
+# which must be specified in the command line
+# Ex. ./canaryPlotter.py dat.dat
+if len(sys.argv)!=2:
+  print "please specify csv file to plot"
+  quit()
+
 # load csv as data frame
 df=pandas.io.parsers.read_csv(str(sys.argv[1]))
 
+# How many columns should I plot?
+numcol=6
+
 # We only want the first 6 collumns
-df = df.ix[:,0:6]
+df = df.ix[:,0:numcol]
 
 # set up plot
 plt.figure() 
-df.plot(alpha=0.3)
-plt.legend(loc='best')
-plt.legend(loc=5,prop={'size':6})
+df.plot(alpha=0.3) # add transparency to see overlapping colors
+plt.legend(loc='best') # add legend in non-intrusive location
+plt.legend(loc=5,prop={'size':numcol}) # 
 plt.ylabel('Current')
 plt.xlabel('Reading #')
 

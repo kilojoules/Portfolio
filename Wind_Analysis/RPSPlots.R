8feea0ffa866da7e36960a5d09203a42d27d0642
kilojoules <jaq8@humboldt.edu>
2014-11-05 01:01:43 -0800

    removed unused plot

--- Wind_Analysis/RPSPlots.R
+++ Wind_Analysis/RPSPlots.R
@@ -46,12 +46,6 @@ legend(11,.2, # places a legend at the appropriate place
         cex=0.7 # Make the legend the right size
         )
 # ----------------------------------------------------
-qplot(comps$power.fork.sub,
-     xlab='Time Period',
-     x = comps$time,
-     ylab='Forecasted Average Power [W]',
-     )
-# ----------------------------------------------------
 # Compare NREL empirical density to
 # Rayleigh distribution fit
 x <- density(NREL.dat$avg.speed)$x

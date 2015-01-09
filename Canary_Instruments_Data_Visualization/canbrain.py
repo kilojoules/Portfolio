06d1164c06e6a9a34f0be8f269fd73cbc4eea430
kilojoules <jaq8@humboldt.edu>
2015-01-08 18:20:27 -0800

    Switched to sklearn

--- Canary_Instruments_Data_Visualization/canbrain.py
+++ Canary_Instruments_Data_Visualization/canbrain.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # Julian Quick
+from sklearn.metrics import accuracy_score
 from pandas import *
 from datetime import datetime
 import numpy as np
@@ -24,13 +25,14 @@ else:
    root = sys.argv[1]
 
 X =[]
+labels = []
+k = 0
 for subdir, dirs, files in os.walk(root):
 
     # plot each file
     for file in files:
 
         if str(file)[-4:]=='.csv': 
-            print file
        
             tp=pandas.io.parsers.read_csv(os.path.join(subdir,file), iterator=True, chunksize=1000)
             df = concat(tp, ignore_index=True)
@@ -43,10 +45,17 @@ for subdir, dirs, files in os.walk(root):
 	    df['Residence']=df2['P1rms (A)']+df2['P2rms (A)']
             df['Specialty']=df2['P3rms (A)']+df2['P4rms (A)']
 
+            k+=1
+            if k == 5: 
+               print "Using data from ",file," for comparrisson. This is not included in the training data."
+               sample = ([df['Residence'].iloc[i:i+n].values for i in df.index[:-n+1]], (df['Specialty'] > 75)[n-1:])
+               continue
+
+            print "training with ",file
             n = 5
             X.extend([df['Residence'].iloc[i:i+n].values for i in df.index[:-n+1]])
-            labels = (df['Specialty'] > 0)[n-1:]
+            labels.extend( (df['Specialty'] > 75)[n-1:])
 
 model = RandomForestClassifier()
-model.fit(X, labels)
-model.predict(X)
+model.fit(sample[0], sample[1])
+print "Accuracy is ",accuracy_score(np.array(model.predict(sample[0])),np.array(sample[1]))

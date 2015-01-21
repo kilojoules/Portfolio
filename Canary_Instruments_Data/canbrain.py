60df9d77852f1a8b1dda27c20c0d75f6ea37da9f
kilojoules <jaq8@humboldt.edu>
2015-01-21 14:50:18 -0800

    recognizes multiple events. Scaled.

--- Canary_Instruments_Data/canbrain.py
+++ Canary_Instruments_Data/canbrain.py
@@ -9,17 +9,25 @@ from sklearn.ensemble import RandomForestClassifier
 import os
 
 numcol=6
-X =[]
-labels = []
+X_DWH =[]
+X_Range =[]
+labels_DWH = []
+labels_Range = []
 k = 0
 
 # number of seconds for each time snippet
 n_DWH = 200
-n = 20
+n_Range = 20
 
+# min height to be considered an event
 maxHeight_DWH = 74.5
-maxHeight = 100
+maxHeight_Range = 100
 
+# mac adress samples
+samples_DWH = {}
+samples_Range = {}
+
+# Check arguments
 if len(sys.argv)<2:
    print 'usage: rootdir'
    quit()
@@ -29,12 +37,25 @@ if len(sys.argv)>2:
 else:
    root = sys.argv[1]
 
-model = RandomForestClassifier()
+# Initialize Random Forest Model
+model = {}
+model['DWH'] = RandomForestClassifier()
+model['Range'] = RandomForestClassifier()
 
 # Iterate through each csv file in root
 for subdir, dirs, files in os.walk(root):
     for file in files:
         if str(file)[-4:]=='.csv': 
+
+            # Record mac adress
+            print subdir
+            mac_adress = subdir.split(os.sep)[1]
+            if mac_adress not in samples_DWH and 'DWH' in mac_adress: 
+               samples_DWH[mac_adress] = []
+               k = 0
+            if mac_adress not in samples_Range and 'Range' in mac_adress: 
+               samples_Range[mac_adress] = []
+               k = 0
        
             # Parse timestamp
             tp=pandas.io.parsers.read_csv(os.path.join(subdir,file), iterator=True, chunksize=100)
@@ -51,30 +72,39 @@ for subdir, dirs, files in os.walk(root):
 
             # Use arbitraily selected file for accuracy sample
             k+=1
-            if k == 5: 
+            if k >= 5 and k <=13: 
                print "Using data from ",file," for accuracy test. This is not included in the training data."
-               sample = (df.Timestamp[n-1:].tolist(),[df['Residence'].iloc[i:i+n].values for i in df.index[:-n+1]], (df['Specialty'] > maxHeight)[n-1:])
+               if 'DWH' in mac_adress:
+                  samples_DWH[mac_adress] += (df.Timestamp[n_DWH-1:].tolist(),[df['Residence'].iloc[i:i+n_DWH].values for i in df.index[:-n_DWH+1]], (df['Specialty'] > maxHeight_DWH)[n_DWH-1:])
+
+               if 'Range' in mac_adress:
+                  samples_Range[mac_adress] = (df.Timestamp[n_Range-1:].tolist(),[df['Residence'].iloc[i:i+n_Range].values for i in df.index[:-n_Range+1]], (df['Specialty'] > maxHeight_Range)[n_Range-1:])
                continue
 
-            print "training with ",file
+            print "training with ",file,'   ' ,mac_adress
 
             # record list of arrays of snippets in time
-            X.extend([df['Residence'].iloc[i:i+n].values for i in df.index[:-n+1]])
+            if 'Range' in mac_adress:
+                X_Range.extend([df['Residence'].iloc[i:i+n_Range].values for i in df.index[:-n_Range+1]])
+                labels_Range.extend( (df['Specialty'] > maxHeight_Range)[n_Range-1:])
+            if 'DWH' in mac_adress:
+                X_DWH.extend([df['Residence'].iloc[i:i+n_DWH].values for i in df.index[:-n_DWH+1]])
+                labels_DWH.extend( (df['Specialty'] > maxHeight_DWH)[n_DWH-1:])
 
             # record True/False results
             # >72.5 was chosen based on visual insepection
-            labels.extend( (df['Specialty'] > maxHeight)[n-1:])
 
 # Make prediction, guage accuracy
 'fitting model...'
-model.fit(X,labels)
+if X_Range: model['Range'].fit(X_Range,labels_Range)
+if X_DWH: model['DWH'].fit(X_DWH,labels_DWH)
 #print "Accuracy is ",accuracy_score(np.array(model.predict(sample[1])),np.array(sample[2]))
 
-def event_detection(day):
+def event_detection(day,ident,event):
    # day is of the form (df.time, df.residential)
    tim = day[0]
    res = day[1]
-   pred = model.predict(res)
+   pred = model[event].predict(res)
 
    P = False
    for i in range(1,len(res)-1):
@@ -82,8 +112,12 @@ def event_detection(day):
          P = True
       if pred[i] == False:
          if P== True: 
-	    print "event on ",tim[i-n/2]
+	    print ident,": event on ",tim[i],'  ',event
  	    P = False
 print 'EVENTS'
 print '==========='
-event_detection((sample[0],sample[1])) 
+for key in samples_DWH:
+   event_detection((samples_DWH[key][0],samples_DWH[key][1]),key,'DWH')
+
+for key in samples_Range:
+   event_detection((samples_Range[key][0],samples_Range[key][1]),key,'Range')
